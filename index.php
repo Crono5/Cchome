0abac76cb4de8366cbc9e0a0d3af467568dfb894
Jorge Jaramillo <mail@jorgejaramillo.com>
2012-01-31 17:45:16 -0500

    Navegación de usuario y barra fixeada

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+  <head> 
+    <meta charset="utf-8"> 
+    <title>Cchome.net</title> 
+    <meta name="description" content=""> 
+    <meta name="author" content=""> 
+ 
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements --> 
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]--> 
+ 
+    <!-- Le styles --> 
+<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+<link rel="stylesheet" href="_/css/style.css">
+    <!-- Le fav and touch icons --> 
+    <link rel="shortcut icon" href="images/favicon.ico"> 
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png"> 
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png"> 
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png"> 
+  </head> 
+ 
+  <body id="bgNuevo"> 
+<div id="contenedorPrincipal"> 
+    <div class="container" > 
+<div class="row"> 
+<div class="span6" id="logo"><h2>COLOMBIA CALIENTE</h2></div> 
+<div class="span10" id="headerDer"> 
+<img src="http://placehold.it/560x100"> 
+</div> 
+</div> 
+</div> 
+ <div class="topbar" data-dropdown="dropdown" id="menuUser">
+      <div class="topbar-inner">
+        <div class="container">
+        	<ul class="nav"><li><a href="#">Publicar</a></li></ul>
+          <ul class="nav secondary-nav">
+          	<li><a href="#">Bienvenido <strong>Visitante</strong></a></li>
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+             </li>
+          </ul>
+          
+        </div>
+      </div><!-- /topbar-inner -->
+    </div>
+     <div class="topbar" data-dropdown="dropdown" id="menuUser">
+      <div class="topbar-inner">
+        <div class="container">
+        	<h3><a href="#" >Colombia Caliente</a></h3>
+            <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            </ul>
+            <ul class="nav secondary-nav">
+            <li><a href="#" class="dropdown-toggle">Categorias</a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Secondary link</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Another link</a></li>
+              </ul></li>
+              </ul>
+              <ul class="nav secondary-nav"><li>
+          <form class="pull-left" action="">
+            <input type="text" placeholder="Buscar"></li></ul>
+          </form>
+          </div>
+      </div><!-- /topbar-inner -->
+    </div>
+    <div class"container"></div>
+<div class="row" id="menuNav"> 
+</div> 
+<div class="span16" id="contenedorHome"> 
+<div class="row"> 
+<div class="span8"> 
+	lista de pos
+</div> 
+<div class="span8"> 
+	<div class="row"> 
+	<div class="span6"><h3>Únete</h3></div> 
+	<div class="span2"><h3>Publicidad</h3></div> 
+	</div> 
+	<div class="row"> 
+	<div class="span4"><h3>hoy</h3></div> 
+	<div class="span4"><h3>semana</h3></div> 
+	</div> 
+	<div class="row"> 
+	<div class="span6"> 
+	<h3>Comentarios</h3> 
+	<div class="row"> 
+	<div class="span6"><h3>Sobre nosotros</h3></div>	
+	</div> 
+	</div> 
+	<div class="span2"><h3>Publicidad</h3></div> 
+	</div> 
+</div> 
+</div> 
+<div class="row"> 
+<div class="span8"> 
+</div> 
+ 
+</div> 
+ 
+      
+ 
+      <footer> 
+        <p>&copy; Company 2011</p> 
+      </footer> 
+ 
+    </div> <!-- /container --> 
+ </div> 
+  </body> 
+</html> 
