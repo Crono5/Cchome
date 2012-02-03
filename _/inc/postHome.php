3b3cabf26461074305c230ed5436ca732b6ed8da
Jorge Jaramillo <mail@jorgejaramillo.com>
2012-02-02 19:55:57 -0500

    Organicé el home, la parte del post

---
+++ _/inc/postHome.php
@@ -0,0 +1,28 @@
+<div class="row" id="postHome">
+		      <div class="span2" id="tumbHome">
+				<ul class="thumbnails">
+					<li class="thumbnail">
+						<a href="#">
+						<img src="http://placehold.it/90x90" alt=""></a>
+						<div class="caption" id="userTumbHome">
+              			<h5><i class="icon-user"></i> <strong><a>Admin</a></strong></h5>
+              			</div>
+						 </li>
+				
+				</ul>
+			  </div>
+				<div class="span4" id="infoPostH">
+					<h3>Pellentesque habitant morbi tristique</h3>
+					<p id="datosPost">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Netus et malesuada fames ac turpis egestas Sol</p>
+					<div class="row">
+						<div class="span2">
+							<p id="datosPost"><i class="icon-heart"></i> Me gusta <strong>1232</strong></p>
+							<p id="datosPost"><i class="icon-time"></i> Fecha <strong></strong>9:03:04</p>
+						</div>
+						<div class="span2">
+							<p id="datosPost"><i class="icon-eye-open"></i> Vistas <strong>1223</strong></p>
+							<p id="datosPost"><i class="icon-comment"></i> Comentarios <strong>0</strong> </p>
+						</div>
+					</div>
+				</div>		
+</div>
