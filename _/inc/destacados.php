32a46d6df5aabb5b674f87404d215c910cdea8bb
Jorge Jaramillo <mail@jorgejaramillo.com>
2012-02-08 17:30:42 -0500

    Botones sociales + Stadísticas

---
+++ _/inc/destacados.php
@@ -0,0 +1,114 @@
+<div class="row widgetBloqueHome" id="destacados">
+				<h2 id="titulosWidgetSidebar">Estadística</h2>
+				<div class="row" id="infoHomeStats">
+					<div class="span3" id="userInfo">
+					<h3>Usuarios</h3>
+					<div class="row">
+						<div class="span3">
+							<div class="span1" id="registradosData">
+								<strong>Registrados</strong>
+								<h4 id="h4Home">8,997</h4>
+							</div>
+							<div class="span1" id="registradosOnline">
+								<strong>Online</strong>
+								<h4 id="h4Home">1,227</h4>
+							</div>
+						</div>
+						<div class="row">
+					<div class="span3" id="ulUsuarioRegistrado">
+							<strong>Último usuario registrado</strong>
+								<h4 id="h4Home">Loncuster11</h4>
+						</div>
+					</div>
+					</div>
+					
+				</div> 
+				<div class="span3" id="userPost">
+					<h3>Publicaciones</h3>
+					<div class="row">
+						<div class="span3">
+							<div class="span1" id="registradosData">
+								<strong>Post</strong>
+								<h4 id="h4Home">18,997</h4>
+							</div>
+							<div class="span1" id="registradosOnline">
+								<strong>Mensajes</strong>
+								<h4 id="h4Home">21,227</h4>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+					<div class="span3" id="usuarioRegistrado">
+							<strong>Comentarios</strong>
+								<h4 id="h4Home">213,988</h4>
+						</div>
+					</div>
+					
+					</div>
+				</div>
+				<div class="row" id="rankGenenral">
+					<h3>Ranking General</h3>
+					<table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Tiempo</th>
+            <th>Usuario</th>
+            <th>Puntos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Día</td>
+            <td>JulianD</td>
+            <td>8,509</td>
+          </tr>
+          <tr>
+            <td>Semana</td>
+            <td>Jacob</td>
+            <td>2,233</td>
+          </tr>
+          <tr>
+          <td>Mes</td>
+            <td>Sara</td>
+            <td>17,900</td>
+          </tr>
+          <tr>
+          <td>Año</td>
+            <td>Loncu</td>
+            <td>172,900</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Ranking General</h3>
+					<table class="table table-striped">
+        <thead>
+          <tr>
+          	<th>Título</th>
+            <th>Puntos</th>
+          </tr>
+         </thead>
+          <tr>
+          	<td>Pellentesque habitant morbi tristique morbi tristique senectu</td>
+          	<td>3,562</td>
+          </tr>
+          <tr>
+          	<td>Plake habitant morbi tristique morbi senectu</td>
+          	<td>3,560</td>
+          </tr>
+          <tr>
+          	<td>morbi tristique senectu</td>
+          	<td>3,533</td>
+          </tr>
+          <tr>
+          	<td>Tristique senectu</td>
+          	<td>3,003</td>
+          </tr>
+          <tr>
+          	<td>Pellentesque haenectu</td>
+          	<td>2,562</td>
+          </tr>
+          
+           </tbody>
+      </table>
+      </div>
+			</div>
--- sidebar.php
+++ sidebar.php
@@ -4,53 +4,51 @@
 				<div class="span4">
 				<h3>Comunidad</h3>
 				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
-				
-				<div id="fb-root"></div>
-				<script>(function(d, s, id) {
-				  var js, fjs = d.getElementsByTagName(s)[0];
-				  if (d.getElementById(id)) return;
-				  js = d.createElement(s); js.id = id;
-				  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=236804413021755";
-				  fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));</script>
-				<div class="fb-like" data-href="http://colombiacaliente.net" data-send="false" data-width="280" data-show-faces="true" data-font="segoe ui"></div>
-				
 				<div class="row">
-					<div class="span2"><a href="https://twitter.com/Masajistas" class="twitter-follow-button" data-show-count="false" data-lang="es" data-size="large">Segui @Masajistas</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div>
-<!-- <div class="span2"><a class="btn" href="#">Hola</a></div> -->
+					<div class="span1" id="fbBtnHome">
+						<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=210325862377755";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://colombiacaliente.net" data-send="false" data-layout="box_count" data-width="75" data-show-faces="false"></div>
+						
+					</div>
+					<div class="span1" id="twBtnHome">
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://colombiacaliente.net" data-count="vertical">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+						
+					</div>
+					<div class="span1" id="gpBtnHome">
+						<!-- Place this tag where you want the +1 button to render -->
+<div class="g-plusone" data-size="tall" data-href="http://colombiacaliente.net"></div>
+
+<!-- Place this render call where appropriate -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'es'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+						
+					</div>
 				</div>
-								
-				<div class="botonpublicar" >
-				<a id="btpubli" href="#" class="btn btn-large btn-success">Publicar</a>
+				<div class="row">
+				<div class="span4" id="mensajePublicar">
+				<p><a class="btn" href="#"><i class="icon-plus"></i> Publicar</a></p>
+				</div>
 				</div>
 				</div>
 				<div class="span2" id="publi1lat">
 				<img src="_/img/ads/ads160x230cc.png"> 
 				</div>
 			</div> 
-			<div class="row widgetBloqueHome" id="destacados">
-				<h2 id="titulosWidgetSidebar">Destacados</h2>
-				<div class="span3">
-					<h3>Hoy</h3>
-					<ul>
-					 <li><a href="#">Pellentesque habitant morbi tristique </a></li>
-					 <li><a href="#">Pellentesque habitant morbi tristique </a></li>
-					 <li><a href="#">Pellentesque habitant morbi tristique </a></li>
-					 <li><a href="#">Pellentesque habitant morbi tristique </a></li>
-					 <li><a href="#">Pellentesque habitant morbi tristique </a></li>
-					</ul>
-				</div> 
-				<div class="span3">
-					<h3>Semana</h3>
-					<ul><li><a href="#">Pellentesque habitant morbi tristique</a></li></ul>
-					<h3>Mes</h3>
-					<ul><li><a href="#">Pellentesque habitant morbi tristique</a></li></ul>
-					<h3>Año</h3>
-					<ul><li><a href="#">Pellentesque habitant morbi tristique</a></li></ul>
-					</div>
-			</div>
+			<?php include '_/inc/destacados.php';?>
 			<!-- widgets mobile -->
 			<div class="row">
 			<div class="span6" id="gridwidgets">
