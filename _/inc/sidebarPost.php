3fb28c94c517d0a94cff2ca3cb391c84c11e0244
Jorge Jaramillo <mail@jorgejaramillo.com>
2012-02-15 21:25:00 -0500

    Cambios con post

---
+++ _/inc/sidebarPost.php
@@ -0,0 +1,32 @@
+<div class="well">
+		<div class="well" id="infoUserPost">
+	<div class="row">			
+<div class="span2">
+	<h3><small>Por: </small><a>Arturito</a></h3>
+ <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+ </div>
+  <div class="span1" id="fotoUserPost">
+ 	<ul class="thumbnails">
+  <li>
+    <a href="#" class="thumbnail">
+      <img src="http://placehold.it/90x90" alt="">
+    </a>
+  </li>
+  </ul>
+	</div>
+		</div>
+		<div class="row">
+ 		<div class="span1">Puntos<small></small></div>
+ 		<div class="span1">Comentarios</div>
+ 		<div class="span1">Es</div>
+ 		</div>
+ 	</div>
+ 	<div class="row">
+ 		<div class="span3">
+ 		<hr>
+ 			like de facebook
+ 		<hr>
+ 		</div>
+ 	</div>
+ 	
+</div>
--- post.php
+++ post.php
@@ -1,8 +1,24 @@
 <?php include 'header.php';?>
+	<div class="container">
 		<div class="row">
-			<div class="span8" id="contenidoPost"></div>
+			<div class="span8" id="contenidoPost">
+				<ul class="breadcrumb">
+					  <li>
+					    <a href="#">Inicio</a> <span class="divider">/</span>
+					  </li>
+					  <li>
+					    <a href="#">Videos xxx</a> <span class="divider">/</span>
+					  </li>
+					  <li class="active">
+					    <a href="#">Video de la mona</a>
+					  </li>
+				</ul>
+				<h1>Titulo del post</h1>
+			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>				
+			</div>
 			<div class="span4" id="sidebarPost">
 				<?php include '_/inc/sidebarPost.php'; ?>	
 				</div>
   	     </div>
+  	   </div>
 <?php include 'footer.php';?>
